4c32504c522329a50d8d1a9911b15149b6f9ec2c
Angel Garcia <angel_dogui@hotmail.com>
2021-01-30 20:51:11 -0600

    Autentificación hacia Spotify.

---
+++ src/utils/AuthenticationHandler/index.js
@@ -0,0 +1,45 @@
+import {authorize, refresh} from 'react-native-app-auth';
+
+class AuthenticationHandler {
+  constructor() {
+    this.spotifyAuthConfig = {
+      clientId: 'e53db68000a149dcbf5d3356862a8d09',
+      clientSecret: '5a26490b3baa465a8132fc069ca0c12f',
+      redirectUrl: 'com.ledroom://oauthredirect',
+      scopes: [
+        'playlist-read-private',
+        'playlist-modify-public',
+        'playlist-modify-private',
+        'user-library-read',
+        'user-library-modify',
+        'user-top-read',
+      ],
+      serviceConfiguration: {
+        authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+        tokenEndpoint: 'https://accounts.spotify.com/api/token',
+      },
+    };
+  }
+
+  async onLogin() {
+    try {
+      const result = await authorize(this.spotifyAuthConfig);
+      alert(JSON.stringify(result));
+      return result;
+    } catch (error) {
+      console.log(JSON.stringify(error));
+    } 
+  }
+
+  async refreshLogin(refreshToken) {
+    const result = await refresh(this.spotifyAuthConfig, {
+      refreshToken: refreshToken,
+    });
+    return result;
+  }
+
+}
+
+const authHandler = new AuthenticationHandler();
+
+export default authHandler;
